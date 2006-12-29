54caa224c95479d30876b46774d89dbe25c89c60
Pascal Leroy <phl+personal@google.com>
2006-12-29 13:29:49 +0000

    Vss2Git

---
+++ Venezia/Scripts/generate_wall.ldraw.bat
@@ -0,0 +1,29 @@
+set PATH=%PATH%;C:\Rational\Apex\playground\lego.ss\wnt.ada95.wrk
+set COLOR=/color ldraw
+set NAME=ldraw
+set SHOW=/skip left_right /skip top_bottom /show tees
+set TRACE=/trace age /trace mozart
+generate_wall %COLOR% %SHOW% %TRACE% /w 22 /h 13 /gen 500 /bottom 1344442 /top 61432141 /tee 1 /tee 7                  /out %NAME%1.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 5  /h 14 /bottom 131                                                           /out %NAME%2.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 6  /h 3  /bottom 1311 /top 141 /tee 1                                          /out %NAME%3.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 20 /h 10 /top 444413 /corner 8 /tee 1 /tee 7 /tee 9                            /out %NAME%4.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 12 /h 2  /bottom 33312 /top 14124 /corner 8 /tee 1 /tee 7 /tee 9               /out %NAME%5.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 6  /h 9  /bottom 123 /top 2211 /corner 2 /tee 1 /tee 3                         /out %NAME%6.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 5  /h 19 /right 0001110001110001110                                            /out %NAME%7.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 3  /h 9  /bottom 12                                                            /out %NAME%8.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 14 /h 2  /top 2121212111 /bottom 12614                                         /out %NAME%9.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 47 /h 19 /gen 5000 /seed 123 /right 0001110001110001110 /corner 22 /corner 24  /out %NAME%10.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 27 /h 19 /gen 5000 /seed 111293 /left 6665554443332221110 /corner 9 /corner 26 /out %NAME%11.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 80 /h 2 /gen 1000 /bottom 231133342221613341423124231113413 /top 2666666636312121212121212111 /corner 9 /corner 26 /corner 55 /corner 57 /out %NAME%12.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 19 /h 23 /left 00011122233344455566677 /right 22222222222222222111000          /out %NAME%13.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 21 /h 1  /bottom 326123121 /corner 18                                          /out %NAME%14.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 57 /h 3  /gen 1000 /bottom  133621331333332163331 /top 3212121212121212123212121212164 /corner 2 /corner 31 /corner 48 /out %NAME%15.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 11 /h 1 /bottom 11111321                                                       /out %NAME%16.ldr
+rem generate_wall %COLOR% %SHOW% %TRACE% /w 26 /h 8 /right 22111000 /bottom 16436123 /corner 15                            /out %NAME%16b.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 78 /h 2  /gen 1000 /seed 111293 /bottom 14111132132121212121212121124421121212121212121 /top 2366466666266634 /corner 17 /corner 21 /corner 46 /corner 53 /tee 17 /tee 20 /tee 22 /tee 45 /tee 52 /tee 54 /out %NAME%17.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 33 /h 1  /bottom 3331231613232 /corner 26                                      /out %NAME%18.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 24 /h 1  /bottom 12141663 /colorseed 56 /parts tiles                           /out %NAME%19.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 21 /h 1  /bottom 123636 /top 46641 /corner 5                                   /out %NAME%20.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 33 /h 1  /bottom 22126131241224 /corner 26 /parts tiles                        /out %NAME%21.ldr
+generate_wall %COLOR% %SHOW% %TRACE% /w 20 /h 1  /bottom 2336411 /corner 5 /parts tiles                                /out %NAME%22.ldr
+
